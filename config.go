6787ee7da9d897e1d101f170a542995f80384faa
Akira Hayashi <smxshxishxad@me.com>
2024-03-17 09:52:12 +0900

    Use envvar as configuration method instead of command-line arguments

--- config.go
+++ config.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"flag"
+	"log"
+	"os"
 	"strings"
+
+	"github.com/caitlinelfring/go-env-default"
 )
 
 type Config struct {
@@ -17,27 +20,27 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	config := &Config{}
-	var repositoryFullName string
+	repositoryFullName := os.Getenv("REPOSITORY_FULL_NAME")
+	token := os.Getenv("TOKEN")
+	addLabel := env.GetBoolDefault("ADD_LABEL", true)
+	defaultLabel := env.GetDefault("DEFAULT_LABEL", "label-required")
+	prefix := env.GetDefault("PREFIX", "prefix")
+	separator := env.GetDefault("SEPARATOR", "/")
+	comment := env.GetDefault("COMMENT", "Add a label with a prefix.")
 
-	flag.StringVar(&repositoryFullName, "repository-full-name", "", "Name of repository in 'owner/repo' format")
-	flag.StringVar(&config.Token, "token", "", "GitHub access token")
-	flag.BoolVar(&config.AddLabel, "add-label", false, "Whether add label or not if the issue did not have label with required prefix")
-	flag.StringVar(&config.DefaultLabel, "default-label", "", "A label to be used when 'add-label=true'")
-	flag.StringVar(&config.Prefix, "prefix", "", "Required label prefix to look for")
-	flag.StringVar(&config.Separator, "separator", "", "Character which separates prefix and label body")
-	flag.StringVar(&config.Comment, "comment", "", "Comment to post when 'add-label=false'")
-
-	flag.Parse()
-
-	if repositoryFullName != "" {
-		parts := strings.Split(repositoryFullName, "/")
-		if len(parts) != 2 {
-			panic("repository-full-name must be in 'owner/repo' format")
-		}
-		config.Owner, config.Repository = parts[0], parts[1]
-
+	parts := strings.Split(repositoryFullName, "/")
+	if len(parts) != 2 {
+		log.Fatalf("REPOSITORY_FULL_NAME must be in 'owner/repo' format, got: %s", repositoryFullName)
 	}
 
-	return config
+	return &Config{
+		Owner:        parts[0],
+		Repository:   parts[1],
+		Token:        token,
+		AddLabel:     addLabel,
+		DefaultLabel: defaultLabel,
+		Prefix:       prefix,
+		Separator:    separator,
+		Comment:      comment,
+	}
 }
