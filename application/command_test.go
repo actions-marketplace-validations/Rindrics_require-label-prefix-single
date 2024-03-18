79a7208d6aca4a01eb0ec5d561c44f5ed6f45781
Akira Hayashi <smxshxishxad@me.com>
2024-03-19 03:16:54 +0900

    Move mocks to `mock.go` to be refered from another test file

--- application/command_test.go
+++ application/command_test.go
@@ -8,49 +8,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type MockExitAction struct {
-	mock.Mock
-}
-
-func (m *MockExitAction) Perform() error {
-	args := m.Called()
-	return args.Error(0)
-}
-
-type MockCommenter struct {
-	mock.Mock
-	PostCommentParams
-}
-
-func (m *MockCommenter) PostComment(p PostCommentParams) error {
-	args := m.Called(p)
-	return args.Error(0)
-}
-
-type MockLabeler struct {
-	mock.Mock
-	AddLabelsParams
-}
-
-func (m *MockLabeler) AddLabels(p AddLabelsParams) error {
-	args := m.Called(p)
-	return args.Error(0)
-}
-
-type MockCommand struct {
-	mock.Mock
-	OnSuccess MockExitAction
-}
-
-func (m *MockCommand) Execute() error {
-	args := m.Called()
-	return args.Error(0)
-}
-
-func (m *MockCommand) Perform() error {
-	return m.Execute()
-}
-
 func TestPostCommentCommand(t *testing.T) {
 	mockCommenter := new(MockCommenter)
 	mockExitAction := new(MockExitAction)
